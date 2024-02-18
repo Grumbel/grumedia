eee2eaf45462fb4afb29af9465bda10bf5d747df
Ingo Ruhnke <grumbel@gmail.com>
2024-02-18 22:18:22 +0100

    Add GPLv3+

--- audio-split.sh
+++ audio-split.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+# grumedia
+# Copyright (C) 2024 Ingo Ruhnke <grumbel@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 set -e
 
 FFMPEG=ffmpeg
@@ -12,7 +28,7 @@ display_help() {
   echo "Usage: $0 [OPTION]... FILE"
   echo "Options:"
   echo "  -h, --help            Display this help message"
-  echo "  -o, --output PATTERN  Write results to PATTERN"
+  echo "  -o, --output PATTERN  Write results to PATTERN (e.g \"out%02d.wav\")"
   echo "  -s, --split SECONDS   Split every SECONDS seconds (default: $SEGMENT_TIME)"
 }
 
